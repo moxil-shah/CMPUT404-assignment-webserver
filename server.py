bbd10ac7d2186ee94c0ae305f688d2d58acf11b5
moxil-shah <shahmv200216@gmail.com>
2022-09-25 11:12:02 -0600

    pass all tests

--- server.py
+++ server.py
@@ -33,8 +33,8 @@ class MyWebServer(socketserver.BaseRequestHandler):
         self.data = self.request.recv(1024).strip()
         self.data = self.data.decode("utf-8")  # decode from binary
         self.data = self.data.split(" ")  # make array from spaces
-        print(self.data)
-        if self.data[0] == 'GET':  # only supported method is GET; all else error 405
+        # print(self.data)
+        if self.data[0] == 'GET':  # only supported method is GET; all else status code 405
             self.handleGET()
             self.request.sendall(bytearray("OK", 'utf-8'))
         else:
@@ -44,43 +44,46 @@ class MyWebServer(socketserver.BaseRequestHandler):
     def handleGET(self):
         # if last char is '/', then path is .../index.html
         if self.data[1][-1] == '/':
-            path = './www'+self.data[1]+'/index.html'
+            path = './www'+self.data[1]+'/index.html' # default to index.html
             contentType = "text/html"
         # if last 4 chars is 'html', then path is the path
         elif self.data[1][-4:] == "html":
             path = './www'+self.data[1]
             contentType = "text/html"
-        # if last 4 chars is 'html', then path is the path
+        # if last 3 chars is 'css', then path is the path
         elif self.data[1][-3:] == "css":
             path = './www'+self.data[1]
             contentType = "text/css"
-         # invalid path
+         # invalid path; add '/' after path
         else:
             header301 = "HTTP/1.1 301 Moved Permanently\r\n"
-            new_location = "Location: " + path + "\r\n"
+            new_location = "Location: " + \
+                self.data[1] + "/\r\n"  # notice the extra '/'
             self.request.sendall(bytearray(header301 + new_location, 'utf-8'))
             return
-            
+
         # serve path
-        
-        # make sure path is actually openable
+
+        # make sure path is actually legit
         try:
-            f = open(path, 'r')
+            f = open(path)
             content = f.read()
             f.close()
-            
+
         # if not, then 404 status code
         except:
             thingsToSend = "HTTP/1.1 404 Not Found\r\n"
-            
+
         # if yes, then serve it
         else:
-            header_200 = "HTTP/1.1 200 OK\r\n"
-            fileType = "Content-Type: " + contentType +"\r\n"
+            header200 = "HTTP/1.1 200 OK\r\n"
+            fileType = "Content-Type: " + contentType + "\r\n"
             contentLen = "Content-Length: " + str(len(content)) + "\r\n\r\n"
-            thingsToSend = header_200 + fileType + contentLen + content
+            thingsToSend = header200 + fileType + contentLen + content
+        # send it all
         finally:
             self.request.sendall(bytearray(thingsToSend, 'utf-8'))
+
 
 if __name__ == "__main__":
     HOST, PORT = "localhost", 8080
