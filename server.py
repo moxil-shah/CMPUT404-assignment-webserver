1b70541508def531302d7aa78d283ebcd898b23f
Moxil Shah <shahmv200216@gmail.com>
2022-09-25 09:43:47 -0600

    fork

--- server.py
+++ server.py
@@ -32,6 +32,7 @@ class MyWebServer(socketserver.BaseRequestHandler):
     def handle(self):
         self.data = self.request.recv(1024).strip()
         print ("Got a request of: %s\n" % self.data)
+        #
         self.request.sendall(bytearray("OK",'utf-8'))
 
 if __name__ == "__main__":
